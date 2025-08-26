278ed763153aa19cf2ac0982dc401856b1419673
seveibar <seveibar@gmail.com>
2025-08-26 14:42:16 -0700

    repro unexpected connectivity keys

---
+++ tests/solvers/SchematicTracePipelineSolver/SchematicTracePipelineSolver_repro01.test.ts
@@ -0,0 +1,111 @@
+import type { InputProblem } from "lib/types/InputProblem"
+import { test, expect } from "bun:test"
+import { SchematicTracePipelineSolver } from "lib/index"
+
+const inputProblem: InputProblem = {
+  chips: [
+    {
+      chipId: "U1",
+      center: { x: 0, y: 0 },
+      width: 1.6,
+      height: 0.6,
+      pins: [
+        {
+          pinId: "U1.1",
+          x: -0.8,
+          y: 0.2,
+        },
+        {
+          pinId: "U1.2",
+          x: -0.8,
+          y: 0,
+        },
+        {
+          pinId: "U1.3",
+          x: -0.8,
+          y: -0.2,
+        },
+        {
+          pinId: "U1.4",
+          x: 0.8,
+          y: -0.2,
+        },
+        {
+          pinId: "U1.5",
+          x: 0.8,
+          y: 0,
+        },
+        {
+          pinId: "U1.6",
+          x: 0.8,
+          y: 0.2,
+        },
+      ],
+    },
+    {
+      chipId: "C1",
+      center: { x: -2, y: 0 },
+      width: 0.5,
+      height: 1,
+      pins: [
+        {
+          pinId: "C1.1",
+          x: -2,
+          y: 0.5,
+        },
+        {
+          pinId: "C1.2",
+          x: -2,
+          y: -0.5,
+        },
+      ],
+    },
+    {
+      chipId: "C2",
+      center: { x: -4, y: 0 },
+      width: 0.5,
+      height: 1,
+      pins: [
+        {
+          pinId: "C2.1",
+          x: -4,
+          y: 0.5,
+        },
+        {
+          pinId: "C2.2",
+          x: -4,
+          y: -0.5,
+        },
+      ],
+    },
+  ],
+  directConnections: [
+    {
+      pinIds: ["U1.1", "C1.1"],
+      netId: "VCC",
+    },
+    {
+      pinIds: ["U1.2", "C2.1"],
+      netId: "EN",
+    },
+  ],
+  netConnections: [
+    {
+      pinIds: ["U1.3", "C2.2", "C1.2"],
+      netId: "GND",
+    },
+  ],
+  availableNetLabelOrientations: {
+    VCC: ["y+"],
+    EN: ["x+", "x-"],
+    GND: ["y-"],
+  },
+  maxMspPairDistance: 2,
+}
+
+test("SchematicTracePipelineSolver_repro01", () => {
+  const solver = new SchematicTracePipelineSolver(inputProblem)
+  solver.solve()
+
+  console.log(solver.schematicTraceLinesSolver!.solvedTracePaths)
+})
