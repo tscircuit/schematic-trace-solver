89b9108bdfaecbccd07bef81644e59a76a985580
seveibar <seveibar@gmail.com>
2025-08-25 21:12:54 -0700

    fix schematic trace lines not being able to go through a chip

--- tests/solvers/SchematicTraceSingleLineSolver/SchematicTraceSingleLineSolver_repro01.test.ts
+++ tests/solvers/SchematicTraceSingleLineSolver/SchematicTraceSingleLineSolver_repro01.test.ts
@@ -6,34 +6,9 @@ test("SchematicTraceSingleLineSolver_repro01", () => {
   const solver = new SchematicTraceSingleLineSolver(input as any)
   solver.solve()
 
-  // TODO check the output to make sure the traces don't go through the chip
-
-  expect(solver.solvedTracePath).toMatchInlineSnapshot(`
-    [
-      {
-        "x": 0.30397715550000004,
-        "y": 0.5800832909999993,
-      },
-      {
-        "x": 0.30397715550000004,
-        "y": 0.7800832909999994,
-      },
-      {
-        "x": 0.3073264555000007,
-        "y": 0.7800832909999994,
-      },
-      {
-        "x": 0.3073264555000007,
-        "y": -0.7800832909999995,
-      },
-      {
-        "x": 0.31067575550000137,
-        "y": -0.7800832909999995,
-      },
-      {
-        "x": 0.31067575550000137,
-        "y": -0.5800832909999993,
-      },
-    ]
-  `)
+  // The solver should fail because it cannot find a path around the chip
+  // without guidelines to help navigate around it
+  expect(solver.failed).toBe(true)
+  expect(solver.error).toBe("No more candidate elbows, everything had collisions")
+  expect(solver.solvedTracePath).toBe(null)
 })
