9a8bef53482530ece7bea4b033f6e3d5cac79999
seveibar <seveibar@gmail.com>
2025-08-26 20:16:15 -0700

    wip MSP reproduction

---
+++ site/MspConnectionPairSolver/MspConnectionPairSolver01.page.tsx
@@ -0,0 +1,16 @@
+import { MspConnectionPairSolver } from "lib/solvers/MspConnectionPairSolver/MspConnectionPairSolver"
+import inputParams from "./MspConnectionPairsSolver01_params.json"
+import { GenericSolverDebugger } from "site/components/GenericSolverDebugger"
+import { useMemo } from "react"
+import type { InputProblem } from "lib/types/InputProblem"
+
+export default () => {
+  const solver = useMemo(
+    () =>
+      new MspConnectionPairSolver({
+        inputProblem: inputParams.inputProblem as unknown as InputProblem,
+      }),
+    [],
+  )
+  return <GenericSolverDebugger solver={solver} />
+}
