a1c69f28618f677d5adfb598505e031b8602c7f7
seveibar <seveibar@gmail.com>
2025-09-02 15:02:53 -0700

    filter all but last step

--- tests/fixtures/matcher.ts
+++ tests/fixtures/matcher.ts
@@ -1,6 +1,16 @@
-import { getSvgFromGraphicsObject } from "graphics-debug"
+import { getSvgFromGraphicsObject, type GraphicsObject } from "graphics-debug"
 import { expect, type MatcherResult } from "bun:test"
 import type { BaseSolver } from "lib/solvers/BaseSolver/BaseSolver"
+
+const getAllElms = (graphicsObject: GraphicsObject) => {
+  return [
+    ...(graphicsObject.lines ?? []),
+    ...(graphicsObject.points ?? []),
+    ...(graphicsObject.rects ?? []),
+    ...(graphicsObject.circles ?? []),
+    ...(graphicsObject.texts ?? []),
+  ]
+}
 
 async function toMatchSolverSnapshot(
   this: any,
@@ -9,6 +19,28 @@ async function toMatchSolverSnapshot(
   svgName?: string,
 ): Promise<MatcherResult> {
   const graphicsObject = received.visualize()
+
+  const allElms = getAllElms(graphicsObject)
+  const lastStep = allElms.reduce((acc, elm) => {
+    return Math.max(acc, elm.step ?? 0)
+  }, 0)
+
+  graphicsObject.points = graphicsObject.points?.filter(
+    (p) => p.step === lastStep,
+  )
+  graphicsObject.lines = graphicsObject.lines?.filter(
+    (l) => l.step === lastStep,
+  )
+  graphicsObject.rects = graphicsObject.rects?.filter(
+    (r) => r.step === lastStep,
+  )
+  graphicsObject.circles = graphicsObject.circles?.filter(
+    (c) => c.step === lastStep,
+  )
+  graphicsObject.texts = graphicsObject.texts?.filter(
+    (t) => t.step === lastStep,
+  )
+
   const svg = getSvgFromGraphicsObject(graphicsObject, {
     backgroundColor: "white",
   })
