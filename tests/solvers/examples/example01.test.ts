caf1238da79134afab6b8616777daa1e7b8e96e3
seveibar <seveibar@gmail.com>
2025-09-02 14:54:47 -0700

    add snapshot and matcher

--- tests/solvers/examples/example01.test.ts
+++ tests/solvers/examples/example01.test.ts
@@ -1,11 +1,12 @@
 import { test, expect } from "bun:test"
 import { SchematicTracePipelineSolver } from "lib/solvers/SchematicTracePipelineSolver/SchematicTracePipelineSolver"
 import { inputProblem } from "site/examples/example01-basic.page"
+import "tests/fixtures/matcher"
 
 test("example01", () => {
   const solver = new SchematicTracePipelineSolver(inputProblem)
 
   solver.solve()
 
-  expect(solver.visualize()).toMatchSvgSnapshot(import.meta.path)
+  expect(solver).toMatchSolverSnapshot(import.meta.path)
 })
