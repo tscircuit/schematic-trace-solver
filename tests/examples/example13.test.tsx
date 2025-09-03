d62c26885754822a5dc58439c05e303b62dec920
imrishabh18 <belikerishabh@gmail.com>
2025-09-03 21:14:51 +0530

    add examples and tests for it

---
+++ tests/examples/example13.test.tsx
@@ -0,0 +1,12 @@
+import { test, expect } from "bun:test"
+import { SchematicTracePipelineSolver } from "lib/solvers/SchematicTracePipelineSolver/SchematicTracePipelineSolver"
+import { inputProblem } from "site/examples/example13.page"
+import "tests/fixtures/matcher"
+
+test("example13", () => {
+  const solver = new SchematicTracePipelineSolver(inputProblem)
+
+  solver.solve()
+
+  expect(solver).toMatchSolverSnapshot(import.meta.path)
+})
