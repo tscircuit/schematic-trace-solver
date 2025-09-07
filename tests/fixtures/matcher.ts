e3b9e95b1b65903df297250fc07b59c01761fb63
seveibar <seveibar@gmail.com>
2025-09-07 15:42:47 -0700

    testing setup

--- tests/fixtures/matcher.ts
+++ tests/fixtures/matcher.ts
@@ -25,21 +25,23 @@ async function toMatchSolverSnapshot(
     return Math.max(acc, elm.step ?? 0)
   }, 0)
 
-  graphicsObject.points = graphicsObject.points?.filter(
-    (p) => p.step === lastStep,
-  )
-  graphicsObject.lines = graphicsObject.lines?.filter(
-    (l) => l.step === lastStep,
-  )
-  graphicsObject.rects = graphicsObject.rects?.filter(
-    (r) => r.step === lastStep,
-  )
-  graphicsObject.circles = graphicsObject.circles?.filter(
-    (c) => c.step === lastStep,
-  )
-  graphicsObject.texts = graphicsObject.texts?.filter(
-    (t) => t.step === lastStep,
-  )
+  if (lastStep !== 0) {
+    graphicsObject.points = graphicsObject.points?.filter(
+      (p) => p.step === lastStep,
+    )
+    graphicsObject.lines = graphicsObject.lines?.filter(
+      (l) => l.step === lastStep,
+    )
+    graphicsObject.rects = graphicsObject.rects?.filter(
+      (r) => r.step === lastStep,
+    )
+    graphicsObject.circles = graphicsObject.circles?.filter(
+      (c) => c.step === lastStep,
+    )
+    graphicsObject.texts = graphicsObject.texts?.filter(
+      (t) => t.step === lastStep,
+    )
+  }
 
   const svg = getSvgFromGraphicsObject(graphicsObject, {
     backgroundColor: "white",
