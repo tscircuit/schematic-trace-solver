ee551e03f51648eed8a6ed3be8008e58c5751de9
0hm☘️ <git8ankanroy@gmail.com>
2025-09-16 05:25:45 +0530

    WIP

--- lib/solvers/TraceLabelOverlapAvoidanceSolver/TraceLabelOverlapAvoidanceSolver.ts
+++ lib/solvers/TraceLabelOverlapAvoidanceSolver/TraceLabelOverlapAvoidanceSolver.ts
@@ -182,15 +182,6 @@ export class TraceLabelOverlapAvoidanceSolver extends BaseSolver {
     if (minimizedTraces) {
       this.updatedTraces = minimizedTraces
     }
-    console.log(
-      this.updatedTraces.map((e) => {
-        return {
-          len: e.tracePath.length,
-          PairIds: e.globalConnNetId,
-        }
-      }),
-    )
-
     const balancedTraces = balanceLShapes({
       traces: this.updatedTraces,
       problem: this.problem,
