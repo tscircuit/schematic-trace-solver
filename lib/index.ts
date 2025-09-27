f6b6a9b9ad6ffe801c73739857ed2a5145b5c99d
Saish Naik <saish9901@gmail.com>
2025-09-27 19:25:48 +0530

    netlabels don't overlap traces  #46

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export * from "./solvers/SchematicTracePipelineSolver/SchematicTracePipelineSolver"
+export * from "./solvers/NetlabelTraceOverlapAvoidanceSolver/NetlabelTraceOverlapAvoidanceSolver"
 export * from "./types/InputProblem"
 export { SchematicTraceSingleLineSolver2 } from "./solvers/SchematicTraceLinesSolver/SchematicTraceSingleLineSolver2/SchematicTraceSingleLineSolver2"
