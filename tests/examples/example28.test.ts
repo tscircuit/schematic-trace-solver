f66c4f5e8790b319d71106f07c5d13a30fb12180
0hm☘️ <git8ankanroy@gmail.com>
2025-10-08 17:47:52 +0530

    WIP

--- tests/examples/example28.test.ts
+++ tests/examples/example28.test.ts
@@ -4,7 +4,6 @@ import { SchematicTracePipelineSolver } from "lib/solvers/SchematicTracePipeline
 import "tests/fixtures/matcher"
 import inputProblem from "../../tests/assets/example25.json"
 
-
 test("example28", () => {
   const solver = new SchematicTracePipelineSolver(inputProblem as any)
 
