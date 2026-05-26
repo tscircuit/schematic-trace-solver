d275c4089e2dbb872a59bb6c571d20dfbee2ca0f
Sangit Manandhar <rux12@icloud.com>
2026-05-26 12:51:01 -0700

    add repro-37 (#443)

---
+++ site/examples/example37.page.tsx
@@ -0,0 +1,4 @@
+import { PipelineDebugger } from "site/components/PipelineDebugger"
+import inputProblem from "../../tests/assets/example37.json"
+
+export default () => <PipelineDebugger inputProblem={inputProblem as any} />
