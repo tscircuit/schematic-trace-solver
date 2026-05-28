dd0f32f3885dcd90f5c079112282115c452f4fa6
Sangit Manandhar <sangit@tscircuit.com>
2026-05-28 11:51:35 -0700

    update tests

--- tests/examples/example41.test.ts
+++ tests/examples/example41.test.ts
@@ -8,33 +8,5 @@ test("example41", () => {
 
   solver.solve()
 
-  const labels = solver.netLabelNetLabelCollisionSolver!.outputNetLabelPlacements
-
-  for (let i = 0; i < labels.length; i++) {
-    for (let j = i + 1; j < labels.length; j++) {
-      const a = labels[i]!
-      const b = labels[j]!
-      if (a.globalConnNetId === b.globalConnNetId) continue
-      const ba = {
-        minX: a.center.x - a.width / 2,
-        maxX: a.center.x + a.width / 2,
-        minY: a.center.y - a.height / 2,
-        maxY: a.center.y + a.height / 2,
-      }
-      const bb = {
-        minX: b.center.x - b.width / 2,
-        maxX: b.center.x + b.width / 2,
-        minY: b.center.y - b.height / 2,
-        maxY: b.center.y + b.height / 2,
-      }
-      const collides =
-        ba.minX < bb.maxX &&
-        ba.maxX > bb.minX &&
-        ba.minY < bb.maxY &&
-        ba.maxY > bb.minY
-      expect(collides).toBe(false)
-    }
-  }
-
   expect(solver).toMatchSolverSnapshot(import.meta.path)
 })
