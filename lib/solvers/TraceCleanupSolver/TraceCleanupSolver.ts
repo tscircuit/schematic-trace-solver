ee1512e15f914a4ced8bc4f037eb8a2458f6944e
KooZuKi <17777685+KooZuKi@users.noreply.github.com>
2026-06-05 00:54:53 -0700

    Disable broad same-net merge tolerance

--- lib/solvers/TraceCleanupSolver/TraceCleanupSolver.ts
+++ lib/solvers/TraceCleanupSolver/TraceCleanupSolver.ts
@@ -103,7 +103,7 @@ export class TraceCleanupSolver extends BaseSolver {
   private _runMergeSameNetTraceLinesStep() {
     this.outputTraces = mergeSameNetTraceLines({
       traces: Array.from(this.tracesMap.values()),
-      maxDistance: this.input.paddingBuffer / 1000,
+      maxDistance: 0,
     })
     this.tracesMap = new Map(this.outputTraces.map((t) => [t.mspPairId, t]))
     this.pipelineStep = "minimizing_turns"
