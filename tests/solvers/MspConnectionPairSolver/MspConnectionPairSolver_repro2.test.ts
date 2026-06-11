b538131d3270af6970ca522d23bc55efa4423ce0
baeltaezaer <info@convierteweb.com.mx>
2026-06-11 00:11:58 -0600

    Prevent net-label-only physical routing

--- tests/solvers/MspConnectionPairSolver/MspConnectionPairSolver_repro2.test.ts
+++ tests/solvers/MspConnectionPairSolver/MspConnectionPairSolver_repro2.test.ts
@@ -1,5 +1,7 @@
 import { MspConnectionPairSolver } from "lib/solvers/MspConnectionPairSolver/MspConnectionPairSolver"
 import { test, expect } from "bun:test"
+import { getConnectivityMapsFromInputProblem } from "lib/solvers/MspConnectionPairSolver/getConnectivityMapFromInputProblem"
+import type { InputProblem } from "lib/types/InputProblem"
 
 test("MspConnectionPairSolver should solve problem correctly", () => {
   const input = {
@@ -89,4 +91,50 @@ test("MspConnectionPairSolver should solve problem correctly", () => {
 
   // Add more specific assertions based on expected output
   // expect(solver.netLabelPlacementSolver!.netLabelPlacements).toMatchInlineSnapshot()
+})
+
+const netLabelOnlyInput: InputProblem = {
+  chips: [
+    {
+      chipId: "schematic_component_0",
+      center: { x: -1, y: 0 },
+      width: 1,
+      height: 1,
+      pins: [{ pinId: "C1.1", x: -0.5, y: 0 }],
+    },
+    {
+      chipId: "schematic_component_1",
+      center: { x: 1, y: 0 },
+      width: 1,
+      height: 1,
+      pins: [{ pinId: "C2.1", x: 0.5, y: 0 }],
+    },
+  ],
+  directConnections: [],
+  netConnections: [{ netId: "GND", pinIds: ["C1.1", "C2.1"] }],
+  availableNetLabelOrientations: {},
+  maxMspPairDistance: 10,
+}
+
+test("net-label-only connections do not mutate the direct connectivity map", () => {
+  const { directConnMap, netConnMap } =
+    getConnectivityMapsFromInputProblem(netLabelOnlyInput)
+  const netOnlyMapId = Object.keys(netConnMap.netMap)[0]!
+
+  expect(Object.keys(directConnMap.netMap)).toHaveLength(0)
+  expect(netConnMap.getIdsConnectedToNet(netOnlyMapId)).toEqual([
+    "GND",
+    "C1.1",
+    "C2.1",
+  ])
+})
+
+test("MspConnectionPairSolver does not route net-label-only connections", () => {
+  const solver = new MspConnectionPairSolver({
+    inputProblem: netLabelOnlyInput,
+  })
+
+  solver.solve()
+
+  expect(solver.mspConnectionPairs).toHaveLength(0)
 })
