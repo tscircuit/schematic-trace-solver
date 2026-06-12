76c7d6559f1741d3c1256b7d9abcb910ff35eec8
IamShreshth <shreshthdhimole@gmail.com>
2026-06-12 13:22:20 +0530

    chore: format code and ensure type safety

--- tests/same-net-merge.test.ts
+++ tests/same-net-merge.test.ts
@@ -5,15 +5,15 @@ import "tests/fixtures/matcher"
 test("SameNetTraceMergeSolver fixes trace jog", () => {
   const unmergedTraces = [
     { id: "t1", net_id: "net_A", x1: 3.6, y1: 0, x2: 3.6, y2: 5 },
-    { id: "t2", net_id: "net_A", x1: 3.5256, y1: 4, x2: 3.5256, y2: 10 }
+    { id: "t2", net_id: "net_A", x1: 3.5256, y1: 4, x2: 3.5256, y2: 10 },
   ]
 
   const solver = new SameNetTraceMergeSolver({
     inputProblem: {} as any,
     traces: unmergedTraces,
   })
-  
-  solver.solve() 
+
+  solver.solve()
 
   expect(solver).toMatchSolverSnapshot(import.meta.path)
 })
