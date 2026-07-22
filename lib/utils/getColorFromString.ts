b7e545769a8faa5363a39b26acd7c9a45944dd46
ThaiRC <thairc@192.168.1.8>
2026-07-22 17:18:13 +0700

    fix: prevent hash overflow to NaN in getColorFromString (#651)

--- lib/utils/getColorFromString.ts
+++ lib/utils/getColorFromString.ts
@@ -1,7 +1,9 @@
 export const getColorFromString = (string: string, alpha = 1) => {
   // pseudo random number from string
   const hash = string.split("").reduce((acc, char) => {
-    return acc * 31 + char.charCodeAt(0)
+    return (acc * 31 + char.charCodeAt(0)) | 0
   }, 0)
-  return `hsl(${hash % 360}, 100%, 50%, ${alpha})`
+  const hue = Math.abs(hash) % 360
+  return `hsl(${hue}, 100%, 50%, ${alpha})`
 }
+
