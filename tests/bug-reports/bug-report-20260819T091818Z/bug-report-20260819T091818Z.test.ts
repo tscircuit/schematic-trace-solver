13ae1bc80c337df7f60e69ce5c69b3aef9e71ecf
Mohan <mohn08052006@gmail.com>
2026-08-19 23:56:56 +0530

    repro disconnected netlabel (#826)
    * up
    
    * up

---
+++ tests/bug-reports/bug-report-20260819T091818Z/bug-report-20260819T091818Z.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "bun:test"
+import { SchematicTracePipelineSolver } from "lib/solvers/SchematicTracePipelineSolver/SchematicTracePipelineSolver"
+import inputProblem from "./bug-report-20260819T091818Z.json"
+import "tests/fixtures/matcher"
+
+test("disconnected netlabel", () => {
+  const solver = new SchematicTracePipelineSolver(inputProblem as any)
+
+  solver.solve()
+
+  expect(solver).toMatchSolverSnapshot(import.meta.path)
+})
