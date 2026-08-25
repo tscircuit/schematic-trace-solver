b137403391054578742ee9ce00025f5609d616d5
Sangit Manandhar <rux12@icloud.com>
2026-08-25 15:09:08 -0700

    test: simplify TIDA-00553 repro

--- site/examples/repro-tida00553-j4-cell-divider.page.tsx
+++
@@ -1,4 +0,0 @@
-import { PipelineDebugger } from "site/components/PipelineDebugger"
-import inputProblem from "../../tests/repros/repro-tida00553-j4-cell-divider.input"
-
-export default () => <PipelineDebugger inputProblem={inputProblem} />
