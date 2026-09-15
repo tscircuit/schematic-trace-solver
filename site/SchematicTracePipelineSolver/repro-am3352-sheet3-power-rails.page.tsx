9ff1661ac7e967a1cc284937b3fb791eb1f384c0
Severin Ibarluzea <seveibar@gmail.com>
2026-09-15 16:37:41 -0700

    test: reproduce AM3352 sheet 3 power and ground rails (#1205)

---
+++ site/SchematicTracePipelineSolver/repro-am3352-sheet3-power-rails.page.tsx
@@ -0,0 +1,7 @@
+import type { InputProblem } from "lib/types/InputProblem"
+import { PipelineDebugger } from "site/components/PipelineDebugger"
+import input from "../../tests/repros/assets/repro-am3352-sheet3-power-rails.input.json"
+
+export default () => (
+  <PipelineDebugger inputProblem={structuredClone(input) as InputProblem} />
+)
