cd13ffa934e6e7db43b17e000427f5fb05761947
mohan-bee <mohn08052006@gmail.com>
2026-09-21 23:01:33 +0530

    up

--- site/SchematicTracePipelineSolver/repro-am3352-03-interfaces.page.tsx
+++
@@ -1,7 +0,0 @@
-import type { InputProblem } from "lib/types/InputProblem"
-import { PipelineDebugger } from "site/components/PipelineDebugger"
-import input from "../../tests/repros/assets/repro-am3352-03-interfaces.input.json"
-
-export default () => (
-  <PipelineDebugger inputProblem={structuredClone(input) as InputProblem} />
-)
